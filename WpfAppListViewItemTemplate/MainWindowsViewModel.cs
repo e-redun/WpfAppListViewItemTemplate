fd23de5c59446e1f291755da6a7473bce4533bbe
redunea <redunea@IN-001232>
2022-12-06 15:14:09 +0500

    Ввел MainWindowViewModel

--- WpfAppListViewItemTemplate/MainWindowsViewModel.cs
+++ WpfAppListViewItemTemplate/MainWindowsViewModel.cs
@@ -6,24 +6,11 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfAppListViewItemTemplate
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public class MainWindowsViewModel : INotifyPropertyChanged
     {
-
         public ObservableCollection<UserModel> Users { get; set; } = new ObservableCollection<UserModel>
         {
             new UserModel(){ FirstName="Валя", SurName="Новикова"},
@@ -46,25 +33,20 @@ namespace WpfAppListViewItemTemplate
         public UserModel SelectedUser
         {
             get { return _SelectedUser; }
-            set {
+            set
+            {
                 _SelectedUser = value;
                 OnPropertyChanged();
 
-            //    MessageBox.Show("SelectedUser.FirstName = " + value.FirstName);
+                //    MessageBox.Show("SelectedUser.FirstName = " + value.FirstName);
             }
         }
 
 
-        public MainWindow()
+        public MainWindowsViewModel()
         {
-
             User = "1111";
-
-            InitializeComponent();
-
-            DataContext = this;
         }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
