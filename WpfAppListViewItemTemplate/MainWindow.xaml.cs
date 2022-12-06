bf86798cc1ce3ed1048c4ba51d7c69b0c25a860b
redunea <redunea@IN-001232>
2022-12-06 15:15:15 +0500

    подчистил - работает

--- WpfAppListViewItemTemplate/MainWindow.xaml.cs
+++ WpfAppListViewItemTemplate/MainWindow.xaml.cs
@@ -24,53 +24,12 @@ namespace WpfAppListViewItemTemplate
     public partial class MainWindow : Window
     {
 
-        public ObservableCollection<UserModel> Users { get; set; } = new ObservableCollection<UserModel>
-        {
-            new UserModel(){ FirstName="Валя", SurName="Новикова"},
-            new UserModel(){ FirstName="Таня", SurName="Шупикова"},
-            new UserModel(){ FirstName="Костя", SurName="Высоцкий"},
-        };
-
-        public List<string> People { get; set; } = new List<string>
-        {
-            "Александр",
-            "Александр",
-            "Евгений"
-        };
-
-        public string User { get; set; } = "Fsdsdfsdf";
-
-
-        private UserModel _SelectedUser;
-
-        public UserModel SelectedUser
-        {
-            get { return _SelectedUser; }
-            set {
-                _SelectedUser = value;
-                OnPropertyChanged();
-
-            //    MessageBox.Show("SelectedUser.FirstName = " + value.FirstName);
-            }
-        }
-
-
         public MainWindow()
         {
 
-            User = "1111";
-
             InitializeComponent();
 
-            DataContext = this;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            DataContext = new MainWindowsViewModel();
         }
     }
 }
