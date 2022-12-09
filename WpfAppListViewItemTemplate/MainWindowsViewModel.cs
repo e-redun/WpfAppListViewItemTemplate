4f5196a8514cae907c79edea08417be58dc6e942
redunea <redunea@IN-001232>
2022-12-09 08:53:13 +0500

    1

--- WpfAppListViewItemTemplate/MainWindowsViewModel.cs
+++ WpfAppListViewItemTemplate/MainWindowsViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WpfAppListViewItemTemplate
 {
@@ -37,8 +38,7 @@ namespace WpfAppListViewItemTemplate
             {
                 _SelectedUser = value;
                 OnPropertyChanged();
-
-                //    MessageBox.Show("SelectedUser.FirstName = " + value.FirstName);
+                MessageBox.Show("SelectedUser.FirstName = " + value.FirstName);
             }
         }
 
